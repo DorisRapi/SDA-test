5a52e7eecd457d1c6fa013c8b165768cd1efe764
Alban Halaj <alban_halaj@live.com>
2022-04-10 12:07:00 +0200

    array commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("====SANJA2====");
+console.log("====Albani====");
+
+const orderNumbers = [9, 1, 3, 7];
+
+function sortNumbers(){
+    orderNumbers.sort (function(a,b){
+        return a-b;
+    });
+    return sortNumbers;
+}
+
+let sortedNumbers = sortNumbers(orderNumbers);
+console.log(sortedNumbers);
+
+let i;
+for (i=0; i<sortedNumbers.length-1; i++){
+    if(sortedNumbers[i+1] - sortedNumbers[i] !==1){
+        console.log("false");
+        break;
+    }
+}
+
+if (i >= sortedNumbers.length-1){
+    console.log("true");
+}
