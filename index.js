ecce46e20d7e502b1601a28cc2e5cc7a83ab05df
dorisrapi <dorisrapi19@gmail.com>
2022-04-10 12:24:54 +0200

    adding comments

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ let i;
 for (i = 0; i < sortedNumbers.length - 1; i++) {
   if (sortedNumbers[i + 1] - sortedNumbers[i] !== 1) {
     console.log("False");
-    break;
+    break; //nese eshte False e ben Break direkt dhe nuk vazhdon me funksionin
   }
 }
 if (i >= sortedNumbers.length - 1) {
+  //ky funksion ekzekutohet pasi ka mbaruar gjithe Loop-i pa u bere asnjehere break.
   console.log("True");
 }
